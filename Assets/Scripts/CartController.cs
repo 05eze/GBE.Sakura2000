38b4e85fa19cfc2e3a62b716c09c01ebd488f583
05eze <esintim2022@elam.co.uk>
2023-04-17 19:18:35 +0100

    Moving shopping cart and added test item

---
+++ Assets/Scripts/CartController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartController : MonoBehaviour
+{
+    public float maxSpeed = 3.0f;
+    Rigidbody2D cartCnt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cartCnt = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movementValueX = Input.GetAxis("Horizontal") * maxSpeed;
+
+        cartCnt.velocity = new Vector2(movementValueX, cartCnt.velocity.y);
+    }
+}
